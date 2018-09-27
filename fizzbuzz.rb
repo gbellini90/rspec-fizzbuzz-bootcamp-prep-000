c9d3c7cd338388de174943b140a7837c9b969c80
Gabrielle Bellini <gbellini90@gmail.com>
2018-09-27 21:05:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,13 @@
 def fizzbuzz(num)
   if num % 3 == 0
     puts "Fizz"
+    
   elsif num % 5 == 0
     puts "Buzz"
+    
   elsif (num % 3 == 0) && (num % 5 == 0)
     puts "FizzBuzz"
+    
   end
+  
 end
